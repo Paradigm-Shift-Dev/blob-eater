83a828fe0b30fef618886e1049ad42f44739f0f4
Martin N. Gregersen <martin@paradigmshift.ee>
2024-01-31 16:07:03 +0100

    0.2.1 - Design update. Readme.

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 ---
 name: Blog Eater
 slug: blob-eater
-description: The Blog Eater, eats blobs... Again and again!
-framework: Next.js
-useCase:
-  - Cron
-  - Edge Functions
-css: Tailwind
-relatedTemplates:
-  - cron-og
+description: `Meet BLOB EATER, the youngest team member of MeinStrain.
+
+While MARI and MIRA are supremely knowledgable and embodied in the form of text and sound interactions IRL with humans, BLOB EATER has a unique purpose, role and embodiment.
+
+PURPOSE, ROLE
+BLOB EATER's purpose is to feed on old blobs (mp3 files) from the AI Avatars: MARI & MIRA. BLOB EATER's role is to balance our overall digital footprint and maintain system-harmony.
+
+EMBODIMENT
+We consider BLOB EATER as a digital house pet at team MeinStrain. Good boy!
+
+FEEDING
+At night, the BLOB EATER awakens and feed on all blobs not too young to die (less than an hour old).`
 ---
 ![BLOB EATER](./public/BLOB-EATER-min.png)
--- app/page.tsx
+++ app/page.tsx
@@ -23,35 +23,48 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.center}>
+        <div>
           <h1>
             BLOB EATER
           </h1>
-          <p>
-            Meet BLOB-EATER, the youngest team member of <a href="https://meinstrain.com/">MeinStrain</a>. It's has the intelligence of a jellyfish, but you can trust it to follow the daily cycle and eats blobs from our server.
-          </p>
-          <Image
-            className={styles.avatar}
-            src="/BLOB-EATER-min.png"
-            alt="BLOB EATER AVATAR"
-            width={512}
-            height={512}
-            priority
-          />
+          <p>Meet BLOB EATER, the youngest team member of <a href="https://meinstrain.com/">MeinStrain</a>.</p>
+          <p>While MARI and MIRA are supremely knowledgable and embodied in the form of text and sound interactions IRL with humans, BLOB EATER has a unique purpose, role and embodiment.</p>
+          <div className={styles.avatarWrapper}> 
+            <Image
+              className={styles.avatar}
+              src="/BLOB-EATER-min.png"
+              alt="BLOB EATER AVATAR"
+              width={512}
+              height={512}
+              priority
+            />
+            <div className={styles.banner}>
+              <a
+              href="/api/edge"
+              className={styles.button}
+              target="_blank"
+              rel="noopener noreferrer"
+              >FEED ME</a>
+               <a
+              href="https://github.com/Paradigm-Shift-Dev/blob-eater.git"
+              className={styles.button}
+              target="_blank"
+              rel="noopener noreferrer"
+              >CLONE ME ON GITHUB</a>
+            </div>
+          </div>
+          <h2>PURPOSE, ROLE</h2>
+          <p>BLOB EATER's purpose is to feed on old blobs (mp3 files) from the AI Avatars: <a href="https://meinstrain.com/">MARI & MIRA</a>. BLOB EATER's role is to balance our overall digital footprint and maintain system-harmony.</p>
+          <h2>EMBODIMENT</h2>
+          <p>We consider BLOB EATER as a digital house pet at team MeinStrain. Good boy!</p>
           <h2>
-            Feeding schedule
+            FEEDING
           </h2>
-          <p>At night, the BLOB EATER awakens and feed on all blobs not too young to die (less than an hour old).</p>
-          <a
-            href="/api/edge"
-            className={styles.button}
-            target="_blank"
-            rel="noopener noreferrer"
-          >Feed now!</a>
+          <p>At night, the BLOB EATER awakens  and feed on all blobs not too young to die (less than an hour old).</p>
         </div>
       </main>
       <footer className={styles.center}>
-        <small>Copyright 2024 © | <a href="https://meinstrain.com/legal-notice">Legal Notice</a> | MeinStrain OÜ | hello (at) meinstrain.com | Version 0.2.0</small>
+        <small>Copyright 2024 © | <a href="https://meinstrain.com/legal-notice">Legal Notice</a> | MeinStrain OÜ | hello (at) meinstrain.com | Version 0.2.1</small>
       </footer>
     </>
   );
